aee7b155b23fb3eb9d359c0143323ba066340a30
thangadurai <thangadurai15g@gmail.com>
2022-08-31 12:34:31 +0530

    make it better

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,8 +1,18 @@
-const config = require('config.json');
-const mongoose = require('mongoose');
-const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions);
-mongoose.Promise = global.Promise;
+// const config = require('config.json');
+// const mongoose = require('mongoose');
+// const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
+// mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions);
+// mongoose.Promise = global.Promise;
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://udhay:passtest@udhaycluster.jje6l.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("users");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 module.exports = {
     User: require('../modules/users/users.model'),
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const errorHandler = require('_helpers/error-handler');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
 
 // use JWT auth to secure the api
 app.use(jwt());
